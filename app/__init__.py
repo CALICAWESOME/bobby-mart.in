6ca34ffb78335f33ff0475a9c827be1fa3697eb1
Martin <martinr4@wit.edu>
2017-06-10 01:43:59 -0400

    add some pretty pictures
    image credit will be added soon

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from os import listdir
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    files = [_ for _ in listdir('static') if _.startswith('m1saka')]
+    return render_template('index.html', files=files)
 
 
 @app.route('/buh')
