325eb5ceff3a17f561592410a94305a4f55a3ed2
CALICAWESOME <greendolphinavenue@gmail.com>
2017-07-15 16:24:05 -0400

    remove text shadow
    - update randomColor function:
      now returns an array of two values, the first being a random color,
      the second being the first element's bitwise inversion

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -2,7 +2,8 @@ var curr = 0;
 var imgs = ['m1saka0.png','m1saka1.png','m1saka2.png','m1saka3.png','m1saka4.png','m1saka5.png','m1saka6.png'];
 $(document).ready(function() {
     fixAboutTab();
-    $('body').css('background-color', randomColor());
+    var ranCo = randomColor();
+    $('body').css('background-color', ranCo[0]);
     cycleColor();
     var $image = $('img'), interval;
     $image.mouseenter(function() {
@@ -20,7 +21,8 @@ $(document).ready(function() {
     $image.click(cycleImg);
 });
 function cycleColor() {
-    $('body').animate({backgroundColor: randomColor()}, 3000, cycleColor);
+    var ranCo = randomColor();
+    $('body').animate({backgroundColor: ranCo[0]}, 3000, cycleColor);
 }
 function cycleImg() {
     // don't show the same image twice in a row
@@ -34,7 +36,8 @@ function cycleImg() {
 // helpers //
 /////////////
 function randomColor() {
-    return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    var c = Math.random()*0xFFFFFF<<0;
+    return ['#'+ c.toString(16), '#'+((~c >>> 0)-0xFF000000).toString(16)].sort();
 }
 function randomRange(max) {
     return Math.floor(Math.random()*max);
