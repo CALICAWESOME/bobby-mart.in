59aacba4b8d0c59c45bbd9e77d8a8c735498f3ae
CALICAWESOME <greendolphinavenue@gmail.com>
2017-07-15 14:41:55 -0400

    un-hardcode about section positioning
    - also reword some things in about section

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,25 +1,26 @@
 var curr = 0;
 var imgs = ['m1saka0.png','m1saka1.png','m1saka2.png','m1saka3.png','m1saka4.png','m1saka5.png','m1saka6.png'];
 $(document).ready(function() {
+    fixAboutTab();
     $('body').css('background-color', randomColor());
-    boi();
-    var image = $('img'), interval;
-    image.hover(function() {
+    cycleColor();
+    var $image = $('img'), interval;
+    $image.hover(function() {
         var x, y, shakeRange = 3;
         interval = setInterval(function() {
             x = randomRange(shakeRange)-(shakeRange/2);
             y = randomRange(shakeRange)-(shakeRange/2);
-            image.css({'-webkit-transform':'translate(' + x + 'px,' + y + 'px)'});
+            $image.css({'-webkit-transform':'translate(' + x + 'px,' + y + 'px)'});
         }, 10);
     },
     function() {
         clearInterval(interval);
         $(this).css({'-webkit-transform':'translate(0,0)'})
     });
-    image.click(cycleImg);
+    $image.click(cycleImg);
 });
-function boi() {
-    $('body').animate({backgroundColor: randomColor()}, 3000, boi);
+function cycleColor() {
+    $('body').animate({backgroundColor: randomColor()}, 3000, cycleColor);
 }
 function cycleImg() {
     // don't show the same image twice in a row
@@ -37,4 +38,11 @@ function randomColor() {
 }
 function randomRange(max) {
     return Math.floor(Math.random()*max);
+}
+////////////
+// fixers //
+////////////
+function fixAboutTab() {
+    var $about = $('#about');
+    $about.css('bottom', '-' + $about.find('.tabBody').outerHeight() +  'px');
 }
