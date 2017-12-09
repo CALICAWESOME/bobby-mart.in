0d5166ee68ced6d60aad561019dc145d563d3f65
CALICAWESOME <greendolphinavenue@gmail.com>
2017-12-09 17:21:39 -0500

    make stuff more better
    - ES6!
    - Make things generally more readable
    - Fix the fucking about tab not being set up correctly sometimes

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,51 +1,42 @@
-var curr = 0;
-var imgs = ['m1saka0.png','m1saka1.png','m1saka2.png','m1saka3.png','m1saka4.png','m1saka5.png','m1saka6.png'];
-$(document).ready(function() {
-    fixAboutTab();
-    var ranCo = randomColor();
-    $('body').css('background-color', ranCo[0]);
+// TODO: can I do an ajax thing to the server to get these instead of hardcoding them?
+const imgs = ['m1saka0.png','m1saka1.png','m1saka2.png','m1saka3.png','m1saka4.png','m1saka5.png','m1saka6.png'];
+let curr = 0, shakeInterval;
+$(window).on('load', () => {
+    // fix about tab
+    let aboutBodyHeight = $('.tabBody').outerHeight();
+    $('#about').css('bottom', -aboutBodyHeight);
+    $('body').css('background-color', randomColor());
+    const image = $('img');
+    image.hover(shakeItUp, stopShaking);
+    image.click(cycleImg);
     cycleColor();
-    var $image = $('img'), interval;
-    $image.mouseenter(function() {
-        var x, y, shakeRange = 3;
-        interval = setInterval(function() {
-            x = randomRange(shakeRange)-(shakeRange/2);
-            y = randomRange(shakeRange)-(shakeRange/2);
-            $image.css({'-webkit-transform':'translate(' + x + 'px,' + y + 'px)'});
-        }, 10);
-    });
-    $image.mouseleave(function() {
-        clearInterval(interval);
-        $(this).css({'-webkit-transform':'translate(0,0)'})
-    });
-    $image.click(cycleImg);
 });
-function cycleColor() {
-    var ranCo = randomColor();
-    $('body').animate({backgroundColor: ranCo[0]}, 3000, cycleColor);
+function shakeItUp() {
+    let x, y, maxShakeDistance = 1.5;
+    const doTheThing = () => {
+        x = randomRange(2 * maxShakeDistance) - maxShakeDistance;
+        y = randomRange(2 * maxShakeDistance) - maxShakeDistance;
+        $(this).css({'-webkit-transform':'translate(' + x + 'px,' + y + 'px)'});
+    };
+    // FUCK EXPLICIT SELECTORS I WANNA USE $(this) GODDAMMIT
+    shakeInterval = setInterval($.proxy(doTheThing, $(this)), 10);
 }
+function stopShaking() {
+    clearInterval(shakeInterval);
+    $(this).css({'-webkit-transform':'translate(0,0)'})
+}
+function cycleColor() { $('body').animate({backgroundColor: randomColor()}, 3000, cycleColor) }
 function cycleImg() {
     // don't show the same image twice in a row
+    let next;
     do {
-        var gen = randomRange(imgs.length);
-    } while (gen === curr);
-    curr = gen;
+        next = randomRange(imgs.length);
+    } while (next === curr);
+    curr = next;
     $(this).attr('src', '/static/' + imgs[curr]);
 }
 /////////////
 // helpers //
 /////////////
-function randomColor() {
-    var c = Math.random()*0xFFFFFF<<0;
-    return ['#'+ c.toString(16), '#'+((~c >>> 0)-0xFF000000).toString(16)].sort();
-}
-function randomRange(max) {
-    return Math.floor(Math.random()*max);
-}
-////////////
-// fixers //
-////////////
-function fixAboutTab() {
-    var $about = $('#about');
-    $about.css('bottom', '-' + $about.find('.tabBody').outerHeight() +  'px');
-}
+function randomColor() { return '#' + (Math.random()*0xFFFFFF<<0).toString(16) }
+function randomRange(max) { return Math.floor(Math.random()*max) }
